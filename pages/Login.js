acc9c9a6cf2b15526003fa4915641445384ae962
MayorFalomo <mayorfalomo@gmailcom>
2022-12-14 13:21:44 +0100

    remove firebase

--- pages/Login.js
+++ pages/Login.js
@@ -1,35 +1,35 @@
-import { signInWithPopup } from "firebase/auth";
-import React, { useContext } from "react";
-import { auth, provider } from "./components/firebase-config";
-import { AppContext } from "./helpers/helpers";
-import { useRouter } from "next/router";
+// import { signInWithPopup } from "firebase/auth";
+// import { useContext } from "react";
+// import { auth, provider } from "./components/firebase-config";
+// import { AppContext } from "./helpers/helpers";
+// import { useRouter } from "next/router";
 
 const Login = () => {
-  const { isAuth, setIsAuth } = useContext(AppContext);
+  // const { isAuth, setIsAuth } = useContext(AppContext);
 
-  const signInWithGoogle = () => {
-    signInWithPopup(auth, provider)
-      .then((res) => {
-        localStorage.setItem(isAuth, true);
-        setIsAuth(true);
-        const router = useRouter();
-        router.push("./");
-        const profilePic = res.user.photoURL;
-        const name = res.user.displayName;
-        const email = res.user.email;
-        localStorage.setItem("name", name);
-        localStorage.setItem("email", email);
-        localStorage.setItem("photoUrl", profilePic);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
+  // const signInWithGoogle = () => {
+  //   signInWithPopup(auth, provider)
+  //     .then((res) => {
+  //       localStorage.setItem(isAuth, true);
+  //       setIsAuth(true);
+  //       const router = useRouter();
+  //       router.push("./");
+  //       const profilePic = res.user.photoURL;
+  //       const name = res.user.displayName;
+  //       const email = res.user.email;
+  //       localStorage.setItem("name", name);
+  //       localStorage.setItem("email", email);
+  //       localStorage.setItem("photoUrl", profilePic);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  // };
   return (
     <div className="logins">
       <div>
         <h2> Welcome to Quiz App </h2>
-        <button onClick={signInWithGoogle}>Continue with Google </button>
+        {/* <button onClick={signInWithGoogle}>Continue with Google </button> */}
         <p>
           This is a one-time password-less login so you don't need a password.{" "}
         </p>
--- pages/components/Firebase-config.js
+++ pages/components/Firebase-config.js
@@ -1,17 +1,18 @@
-import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
+// import * as firebase from 'firebase';
+// import { initializeApp } from "firebase/app";
+// import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDc8w4Z0VbMVpL_-E-NVwUVYydCqGD5nWI",
-  authDomain: "quiz-app-286d4.firebaseapp.com",
-  projectId: "quiz-app-286d4",
-  storageBucket: "quiz-app-286d4.appspot.com",
-  messagingSenderId: "298224485534",
-  appId: "1:298224485534:web:78361b50e5032365ac678f",
-  measurementId: "G-PPW2CWYKZ5",
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDc8w4Z0VbMVpL_-E-NVwUVYydCqGD5nWI",
+//   authDomain: "quiz-app-286d4.firebaseapp.com",
+//   projectId: "quiz-app-286d4",
+//   storageBucket: "quiz-app-286d4.appspot.com",
+//   messagingSenderId: "298224485534",
+//   appId: "1:298224485534:web:78361b50e5032365ac678f",
+//   measurementId: "G-PPW2CWYKZ5",
+// };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const provider = new GoogleAuthProvider();
+// const app = initializeApp(firebaseConfig);
+// export const auth = getAuth(app);
+// export const provider = new GoogleAuthProvider();
