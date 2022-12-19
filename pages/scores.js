cbf48ef9eda841d8cb1f585aa4ce3b3f61f59947
MayorFalomo <mayorfalomo@gmailcom>
2022-12-19 10:16:10 +0100

    comments

--- pages/scores.js
+++ pages/scores.js
@@ -15,9 +15,9 @@ const scores = () => {
   return (
     <div id={theme} data-aos="zoom-in-up" className={styles.container}>
       <h1>Your score is {score} / 10 </h1>
-      {/* <p>{score <= 4 ? "You're not smart afterall lol." : ""}</p>
-      <p>{score == 5 ? "You're just average, Do Better!!." : ""}</p>
-      <p>{score > 5 ? "Seems like you're Smart afterall, Congrats!." : ""}</p> */}
+      <p>{score <= 4 ? "You're not smart afterall lol." : ""}</p>
+      <p>{score === 5 ? "You're just average, Do Better!!." : ""}</p>
+      <p>{score > 5 ? "Seems like you're Smart afterall, Congrats!." : ""}</p>
       <Link href="./">
         <button className={styles.restartBtn}>Restart Quiz </button>{" "}
       </Link>
